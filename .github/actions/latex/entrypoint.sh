524875b7f831811639088eb398d17c3f70b390aa
Mackyson <mackyson72@gmail.com>
2021-01-08 22:01:17 +0900

    pull requestをopenしたら自動でpdfをrelease

---
+++ .github/actions/latex/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eux
+
+# build pdf (change if necessary)
+platex thesis.tex -synctex=1 -halt-on-error -file-line-error %O %S
+dvipdfmx thesis.dvi
+
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+-d "
+{
+  \"tag_name\": \"v$GITHUB_SHA\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"v$GITHUB_SHA\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=thesis.pdf\
+  --header 'Content-Type: application/pdf'\
+  --upload-file thesis.pdf
